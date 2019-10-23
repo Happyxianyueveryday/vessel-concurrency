efb05cc5dcf3db156b257ecac7f9f8a97ef1db1b
pzh <1972508489@qq.com>
2019-10-23 14:04:51 +0800

    Add files via upload

---
+++ src/Werkzurg/SpinLock/CLHLock/test/单元测试.cpp
@@ -0,0 +1,61 @@
+#include "CLHLock.h"  
+#include <iostream>        
+#include <string>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+ 
+CLHLock lk;
+
+//mutex mut;
+
+void th1_func() 
+{
+	//lock_guard<mutex> guard(mut);
+	for(int i=0;i<100;i++)
+	{
+		lk.lock();
+		cout<<"thread1"<<" output"<<endl;
+		cout<<"thread1"<<" output"<<endl;
+		cout<<"thread1"<<" output"<<endl;
+		lk.unlock();
+	} 
+}
+
+void th2_func() 
+{
+	//lock_guard<mutex> guard(mut);
+	for(int i=0;i<200;i++)
+	{
+		lk.lock();
+		cout<<"thread2"<<" output"<<endl;
+		cout<<"thread2"<<" output"<<endl;
+		cout<<"thread2"<<" output"<<endl;
+		lk.unlock();
+	}
+}
+
+void th3_func() 
+{
+	//lock_guard<mutex> guard(mut);
+	for(int i=0;i<300;i++)
+	{
+		lk.lock();
+		cout<<"thread3"<<" output"<<endl;
+		cout<<"thread3"<<" output"<<endl;
+		cout<<"thread3"<<" output"<<endl;
+		lk.unlock();
+	}
+}
+
+int main(void)
+{
+	thread th1(th1_func);
+	thread th2(th2_func);
+	thread th3(th3_func);
+	
+	th1.join();
+	th2.join();
+	th3.join();
+}
